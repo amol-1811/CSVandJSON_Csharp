9bfb77db55eaad3f6d4476c740cf228f19c42bd2
Amol Gosavi <gosaviamol1811@gmail.com>
2025-02-27 17:09:00 +0530

    uncomment the required part

--- Program.cs
+++ Program.cs
@@ -14,31 +14,31 @@ public class Program
     public static void Main(string[] args)
     {
 
-        JsonFile.JsonReader();
-    //    var person = new List<Person>
-    //    {
-    //        new Person{Id=101, Name= "Amol", Age=24},
-    //        new Person{Id=102, Name= "Rishi", Age=22},
-    //        new Person{Id=103, Name= "Tannie", Age=23},
-    //    };
+        //JsonFile.JsonReader();
+            var person = new List<Person>
+            {
+                new Person{Id=101, Name= "Amol", Age=24},
+                new Person{Id=102, Name= "Rishi", Age=22},
+                new Person{Id=103, Name= "Tannie", Age=23},
+            };
 
-    //    string path = @"C:\Users\amol-gosavi\source\repos\amol-1811\CSVandJSON\CSVandJSON\Person.csv";
+             string path = @"C:\Users\amol-gosavi\source\repos\amol-1811\CSVandJSON\CSVandJSON\Person.csv";
 
-    //    //File.Create(path);
-    //    //using (var writer = new  StreamWriter(path))
-    //    //    using (var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
-    //    //{
-    //    //    csvWriter.WriteRecords(person);
-    //    //}
+            //File.Create(path);
+            //using (var writer = new  StreamWriter(path))
+            //    using (var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            //{
+            //    csvWriter.WriteRecords(person);
+            //}
 
-    //    using (var reader = new StreamReader(path))
-    //    using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
-    //    {
-    //        var records = csvReader.GetRecords<Person>();
-    //        foreach (var record in records)
-    //        {
-    //            Console.WriteLine($"PersonID={record.Id}, PersonName={record.Name}, PersonAge={record.Age}");
-    //        }
-    //    }
+            using (var reader = new StreamReader(path))
+            using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csvReader.GetRecords<Person>();
+                foreach (var record in records)
+                {
+                    Console.WriteLine($"PersonID={record.Id}, PersonName={record.Name}, PersonAge={record.Age}");
+                }
+            }
     }
 }
