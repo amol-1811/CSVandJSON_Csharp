8d0f31d940a46d84f4705382c81a6f7a46f7581d
Amol Gosavi <gosaviamol1811@gmail.com>
2025-02-27 17:07:32 +0530

    csv and json file created and read

---
+++ JsonFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using static Program;
+
+namespace CSVandJSON
+{
+    public class JsonFile
+    {
+        public static void JsonReader()
+        {
+            var person = new List<Person>
+            {
+                new Person{Id=101, Name= "Amol", Age=24},
+                new Person{Id=102, Name= "Rishi", Age=22},
+                new Person{Id=103, Name= "Tannie", Age=23},
+            };
+
+            string path = @"C:\Users\amol-gosavi\source\repos\amol-1811\CSVandJSON\CSVandJSON\Person.json";
+
+            //File.Create(path);
+            string jsonData = JsonConvert.SerializeObject(person, Formatting.Indented);
+            File.WriteAllText(path, jsonData);
+
+            string jsonRead = File.ReadAllText(path);
+            var deserializedData = JsonConvert.DeserializeObject<List<Person>>(jsonRead);
+            foreach(var per in deserializedData)
+            {
+                Console.WriteLine($"PerID={per.Id}, PerName={per.Name}, PerAge={per.Age}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using CSVandJSON;
+using CsvHelper;
+
+public class Program
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public static void Main(string[] args)
+    {
+
+        JsonFile.JsonReader();
+    //    var person = new List<Person>
+    //    {
+    //        new Person{Id=101, Name= "Amol", Age=24},
+    //        new Person{Id=102, Name= "Rishi", Age=22},
+    //        new Person{Id=103, Name= "Tannie", Age=23},
+    //    };
+
+    //    string path = @"C:\Users\amol-gosavi\source\repos\amol-1811\CSVandJSON\CSVandJSON\Person.csv";
+
+    //    //File.Create(path);
+    //    //using (var writer = new  StreamWriter(path))
+    //    //    using (var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
+    //    //{
+    //    //    csvWriter.WriteRecords(person);
+    //    //}
+
+    //    using (var reader = new StreamReader(path))
+    //    using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+    //    {
+    //        var records = csvReader.GetRecords<Person>();
+    //        foreach (var record in records)
+    //        {
+    //            Console.WriteLine($"PersonID={record.Id}, PersonName={record.Name}, PersonAge={record.Age}");
+    //        }
+    //    }
+    }
+}
